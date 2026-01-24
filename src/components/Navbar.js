2ec4a2e3751e5e478ffd5041723a0c26e456c147
faideclothing <faideclothingsa@gmail.com>
2026-01-24 04:51:02 +0200

    Add Navbar component with navigation links

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="faide-wordmark">FAIDE</div>
+      <div className="nav-links">
+        <a href="#lookbook">Lookbook</a>
+        <a href="#shop">Shop</a>
+        <a href="#policies">Policies</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
